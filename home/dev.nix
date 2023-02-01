7e961a4624a02c91fbd6a0d58d15fcdbe9b08127
Edhebi <19776499+edhebi@users.noreply.github.com>
2023-02-01 12:28:49 +0100

    use nixpkgs' function to generate vscode's extension.json

--- home/dev.nix
+++ home/dev.nix
@@ -16,7 +16,6 @@
 			mvllow.rose-pine
 			brunnerh.insert-unicode
 		];
-		mutableExtensionsDir = true;
 		userSettings = {
 			"window.menuBarVisibility" = "toggle";
 
@@ -26,22 +25,15 @@
 			"files.insertFinalNewline" = true;
 			"editor.insertSpaces" = false;
 			"editor.fontLigatures" = true;
+
+			"files.associations"."*.md.html" = "markdown";
 		};
 	};
-	home.file.".vscode/extensions/extensions.json".text = let
-		extensionDir = "${config.home.homeDirectory}/.vscode/extensions/";
-		extensionPath = drv: "${extensionDir}${drv.vscodeExtUniqueId}";
-		extensionEntry = drv: rec {
-			identifier.id = drv.vscodeExtUniqueId;
-			version = drv.version;
-			location = {
-				"$mid" = 1;
-				fsPath = extensionPath drv;
-				path = location.fsPath;
-				scheme = "file";
-			};
-		};
-	in builtins.toJSON (map extensionEntry config.programs.vscode.extensions);
+
+	/* See https://github.com/nix-community/home-manager/issues/3507 */
+	programs.vscode.mutableExtensionsDir = false;
+	home.file.".vscode/extensions/extensions.json".text =
+		pkgs.vscode-utils.toExtensionJson config.programs.vscode.extensions;
 
 	programs.git = {
 		enable = true;
@@ -70,6 +62,4 @@
 			confirm_os_window_close = -2;
 		};
 	};
-
-	
 }
